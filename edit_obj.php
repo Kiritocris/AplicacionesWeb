1ed63ca92958b61b0a6b5e897ac93552872e030a
Cristhian Cortes <spiderman4122010@hotmail.com>
2024-12-07 01:45:38 -0600

    UpdateButton

---
+++ edit_obj.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+// Conexión a la base de datos
+$link = mysqli_connect("localhost", "root", "", "gestion_inventario");
+
+if (!$link) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+// Obtener datos del formulario
+$id = $_POST['id'];
+$nombre = mysqli_real_escape_string($link, $_POST['nombre']);
+$descripcion = mysqli_real_escape_string($link, $_POST['descripcion']);
+$categoria = mysqli_real_escape_string($link, $_POST['categoria']);
+$cantidad = intval($_POST['cantidad']);
+$precio = floatval($_POST['precio']);
+
+// Actualizar el registro en la base de datos
+$sql = "UPDATE articulo SET 
+            nombre = '$nombre',
+            descripcion = '$descripcion',
+            categoria = '$categoria',
+            cantidad = '$cantidad',
+            precio = '$precio'
+        WHERE id = '$id'";
+
+if (mysqli_query($link, $sql)) {
+    // Redireccionar después de actualizar
+    header('Location: index.php');
+} else {
+    echo "Error al actualizar el artículo: " . mysqli_error($link);
+}
+
+// Cerrar la conexión
+mysqli_close($link);
+?>
